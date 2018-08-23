de5bbd883c70c8ebdc69cac0fa5db6a73f7ef34c
Fan Wu <fanwu0124@gmail.com>
2018-08-23 10:41:45 -0400

    Added the first redux form field.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderTitleField(field) {
+    return (
+      <div>
+        <input
+          type="text"
+          //This lets all properties of field to be communicatedd as props to the input tag
+          //It's like adding the following props into the input tag:
+          //onChange={field.input.onChnage}
+          //onFocus={field.input.onFocus}
+          //onBlur={field.input.onBlur}
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
-        <div>
-          PostsNew
-        </div>
+        <form>
+          <Field
+            name="title"
+            component={ this.renderTitleField }
+          />
+        </form>
     );
   };
 }
 
-export default PostsNew;
+//Connect component with the form reducer.
+export default reduxForm({
+  form: 'PostsNewForm' //The form name must be unique.
+ })(PostsNew);
