b89b8cb03819ca3719bf37cf7773b782ef28f081
Fan Wu <fanwu0124@gmail.com>
2018-08-23 09:35:00 -0400

    A bug about route and explanation.

--- src/index.js
+++ src/index.js
@@ -7,15 +7,21 @@ import ReduxPromise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 //Must wrap Route inside div
+//http://localhost:8080/posts/new will render both components.
+//Because Route loosely matches the path.
+//http://localhost:8080/posts/new has '/' after the domain, then
+//it returnes all the components which have '/' after the domain.
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
         <Route path="/" component={PostsIndex} />
+        <Route path="/posts/new" component={PostsNew} />
       </div>
     </BrowserRouter>
   </Provider>
