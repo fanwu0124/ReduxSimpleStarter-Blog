96f11aeadf65e28cdd338e70f8aabee0913b4009
Fan Wu <fanwu0124@gmail.com>
2018-08-28 11:57:58 -0400

    Added action creator for new post.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
 
@@ -30,7 +32,7 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    console.log(values);
+    this.props.createPost(values);
   }
 
   render() {
@@ -89,4 +91,6 @@ function validate(values) {
 export default reduxForm({
   validate, //same as validate: validate,
   form: 'PostsNewForm' //The form name must be unique.
- })(PostsNew);
+ })(
+   connect(null, { createPost })(PostsNew) //Pass connect to reduxForm.
+ );
